a5f94e47ce846d25a77583ab27f13a7b4fdbfcda
DzG <ifellous01@gmail.com>
2023-03-13 20:31:21 +0100

    user in pogres

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
@@ -16,9 +17,11 @@ class LoginController extends AbstractController
 
          // last username entered by the user
          $lastUsername = $authenticationUtils->getLastUsername();
+         
         return $this->render('login/index.html.twig', [
                          'last_username' => $lastUsername,
                          'error'         => $error,
         ]);
     }
+    
 }
