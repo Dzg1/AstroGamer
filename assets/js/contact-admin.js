4cc94d3d5986a2ac6de34b8876bb5750ad1782fa
DzG <ifellous01@gmail.com>
2023-03-23 21:29:31 +0100

    mail-admin in progress

--- assets/js/contact-admin.js
+++ assets/js/contact-admin.js
@@ -1,2 +1,23 @@
-import '../styles/contact-admin.scss';
+import "../styles/contact-admin.scss";
 console.log("is app_contact_admin");
+
+/*** troncage du content  ***/
+
+let contents = document.querySelectorAll(".contents");
+
+function truncateText(textContainer) {
+  var maxWidth = textContainer.offsetWidth;
+  var text = textContainer.textContent;
+
+  while (textContainer.offsetWidth > maxWidth) {
+    text = text.slice(0, -1);
+    textContainer.textContent = text + "...";
+    console.log("is truncate"); 
+  }
+}
+
+contents.forEach((content) => {
+    content.style.width = "100%";
+  truncateText(content);
+
+});
