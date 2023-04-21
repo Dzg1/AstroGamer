9f902a2d33883c9e51eb9969be0e727679948278
DzG <ifellous01@gmail.com>
2023-04-21 20:45:01 +0200

    hangman done reste points

--- assets/js/hangman-game.js
+++ assets/js/hangman-game.js
@@ -13,7 +13,7 @@ let img = document.querySelector("#img");
 let game = document.querySelector("#game");
 let gameText = document.querySelector("#game-text");
 let used = document.querySelector("#used");
-let keyContain= document.querySelector('#key-contain');
+let keyContain = document.querySelector("#key-contain");
 
 let tableToFind = [];
 let keyTested = undefined;
@@ -22,33 +22,34 @@ let pv = 9;
 let win = 0;
 let itsALetter = false;
 let imgNbr = 0;
+let keys = [];
 let azerty = [
   "a",
-  "b",
-  "c",
-  "d",
+  "z",
   "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "q",
+  "s",
+  "d",
   "f",
   "g",
   "h",
-  "i",
   "j",
   "k",
   "l",
   "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
   "w",
   "x",
-  "y",
-  "z",
+  "c",
+  "v",
+  "b",
+  "n",
 ];
 
 // ajax
@@ -59,7 +60,7 @@ xhr.onload = function () {
     let response = JSON.parse(xhr.responseText);
     let word = response.word;
     let tableWord = word.split("");
-    console.log(response.word); 
+    console.log(response.word);
 
     function testingLetter(letterToTest, word) {
       let count = 0;
@@ -78,41 +79,98 @@ xhr.onload = function () {
     }
 
     function gameLauncher() {
+      let row1 = document.createElement("div");
+      row1.classList.add("row");
+      row1.classList.add("row-1");
+      let row2 = document.createElement("div");
+      row2.classList.add("row");
+      row2.classList.add("row-2");
+      let row3 = document.createElement("div");
+      row3.classList.add("row");
+      row3.classList.add("row-3");
+
+      azerty.forEach((letter) => {
+        let key = document.createElement("div");
+        keys.push(key);
+        key.setAttribute("id", letter);
+        key.classList.add("key");
+        key.innerHTML = letter;
+        if (azerty.indexOf(letter) < 10) {
+          row1.appendChild(key);
+        } else if (azerty.indexOf(letter) < 19) {
+          row2.appendChild(key);
+        } else {
+          row3.appendChild(key);
+        }
+
+        key.addEventListener("click", (event) => {
+          key.style.background = "grey";
+          console.log(letter + " est cliqué");
+          keyTested = letter;
+          azerty.forEach((letter) => {
+            if (letter === keyTested) {
+              return (itsALetter = true);
+            }
+          });
+          if (itsALetter === true) {
+            testingLetter(keyTested, tableWord);
+            wordToFind.innerHTML = tableToFind.join(" ");
+            img.src = `../images/hangman/hang${imgNbr}.png`;
+          }
+          if (pv === 0) {
+            gameOver.style.display = "block";
+            btnReload.style.display = "block";
+            gOWord.innerHTML = word;
+            gameText.style.display = "none";
+          }
+          if (win === word.length) {
+            victory.style.display = "block";
+            btnReload.style.display = "block";
+            gameText.style.display = "none";
+          }
+        });
+      });
+      keyContain.appendChild(row1);
+      keyContain.appendChild(row2);
+      keyContain.appendChild(row3);
+
       for (let i = 0; i < tableWord.length; i++) {
         tableToFind[i] = "_";
         wordToFind.innerHTML = tableToFind.join(" ");
-        azerty.forEach((letter) => {
-          let key = document.createElement("div");
-          key.innerHTML = letter;
-         keyContain.appendChild(key);
-        });
       }
     }
+    console.log(keys);
     gameLauncher();
-
     window.addEventListener("keydown", (event) => {
-      keyTested = event.key;
-        azerty.forEach((letter) => {
-        if (letter === keyTested) {
-          return (itsALetter = true);
+      keys.forEach((key) => {
+
+        if (key.textContent === event.key) {
+
+          key.style.background = "grey";
+          keyTested = event.key;
+          azerty.forEach((letter) => {
+            if (letter === keyTested) {
+              return (itsALetter = true);
+            }
+          });
+          if (itsALetter === true) {
+            testingLetter(keyTested, tableWord);
+            wordToFind.innerHTML = tableToFind.join(" ");
+            img.src = `../images/hangman/hang${imgNbr}.png`;
+          }
+          if (pv === 0) {
+            gameOver.style.display = "block";
+            btnReload.style.display = "block";
+            gOWord.innerHTML = word;
+            gameText.style.display = "none";
+          }
+          if (win === word.length) {
+            victory.style.display = "block";
+            btnReload.style.display = "block";
+            gameText.style.display = "none";
+          }
         }
       });
-      if (itsALetter === true) {
-        testingLetter(keyTested, tableWord);
-        wordToFind.innerHTML = tableToFind.join(" ");
-        img.src = `../images/hangman/hang${imgNbr}.png`;
-      }
-      if (pv === 0) {
-        gameOver.style.display = "block";
-        btnReload.style.display = "block";
-        gOWord.innerHTML = word;
-        gameText.style.display = "none";
-      }
-      if (win === word.length) {
-        victory.style.display = "block";
-        btnReload.style.display = "block";
-        gameText.style.display = "none";
-      }
     });
 
     btnReload.addEventListener("click", () => {
